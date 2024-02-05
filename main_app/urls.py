d9ff24bb97be3f8bdf7a9ef2bfd8bdff56a011d5
Aldiana <hotaldiana@gmail.com>
2024-02-05 16:48:14 -0500

    models and serializers

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 # import Home view from the views file
-from .views import Home
+from .views import Home, ShoeList, ShoeDetail
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
+  path('shoes/', ShoeList.as_view(), name='shoe-list'),
+  path('shoes/<int:id>/', ShoeDetail.as_view(), name='shoe-detail'),
 ]
